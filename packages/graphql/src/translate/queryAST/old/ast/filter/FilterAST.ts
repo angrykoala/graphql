f1155ccccd36d417e1ab66c14e236419f15b687e
angrykoala <angrykoala@outlook.es>
2023-03-30 17:59:12 +0100

    Remove old query ast

--- packages/graphql/src/translate/queryAST/old/ast/filter/FilterAST.ts
+++
@@ -1,6 +0,0 @@
-import type Cypher from "@neo4j/cypher-builder";
-import { QueryASTNode } from "../../QueryASTNode";
-
-export abstract class FilterAST extends QueryASTNode {
-    abstract getPredicate(node: Cypher.Node): Cypher.Predicate | undefined;
-}
